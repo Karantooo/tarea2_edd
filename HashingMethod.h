0fef4bbc15ea37c73adb8862a6c2e4eaa812d062
sknz-lara <martin.lara.tsx@gmail.com>
2024-05-23 22:19:49 -0400

    feat: clase HashingMethod con funciones para calcular la posicion de un dato

---
+++ HashingMethod.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+
+class HashingMethod {
+    private:
+
+    float A = (sqrt(5) - 1) / 2;
+
+    public:
+    // Funciones Hash para determinar a que bucket va el dato
+    int hashf1(int k, int n) {
+        return k % n;
+    }
+    int hashf2(int k, int n) {
+        float a = (float)k*A;
+        a -= (int)a;
+        return n*a;
+    }
+    // Funciones de los distintos metodos de Hashing
+
+    int linearProbing(int k, int n, int i) {
+        return (hashf1(k, n) + i) % n;
+    }
+
+    int quadraticProbing(int k, int n, int i) {
+        return (hashf1(k, n) + i + 2 * i * i) % n;
+    }
+
+    int doubleHashing(int k, int n, int i) {
+        return (hashf1(k, n) + i * (hashf2(k, n) + 1)) % n;
+    }
+};
