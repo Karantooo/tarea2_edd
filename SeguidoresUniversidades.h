4834a8e9f87db109126f624bf5478f53cb14fe2c
karantooo <ctomasalvarezn@gmail.com>
2024-05-25 16:03:51 -0400

    feat: se agrega atributos a clase para las celdas

--- SeguidoresUniversidades.h
+++ SeguidoresUniversidades.h
@@ -1,3 +1,13 @@
+#include <string>
+
 class SeguidoresUniversidades{
+    public:
+        std::string university;
+        long long user_id;
+        std::string user_name;
+        int number_tweets;
+        int friends_count;
+        int followers_count;
+        std::string created_at;
 
 };
