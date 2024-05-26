c3f7f81b2ca9b0967e371170e08a2bd8137923e7
karantooo <ctomasalvarezn@gmail.com>
2024-05-26 10:06:59 -0400

    feat: se añade put y get para claves long long

--- MapADT.h
+++ MapADT.h
@@ -7,7 +7,7 @@ enum opcion_de_clave{
 };
 
 class MapADT{
-    private:
+    protected:
         opcion_de_clave opcion;
 
     public:
@@ -17,21 +17,21 @@ class MapADT{
 
         virtual void put(SeguidoresUniversidades usuarios) = 0;
         //metodos para user ID
-        virtual SeguidoresUniversidades& get(long long key) = 0;
-        virtual SeguidoresUniversidades& remove(long long key) = 0;
+        virtual SeguidoresUniversidades get(long long key) = 0;
+        virtual SeguidoresUniversidades remove(long long key) = 0;
 
         //metodos para user name
-        virtual SeguidoresUniversidades& get(std::string key) = 0;
-        virtual SeguidoresUniversidades& remove(std::string key) = 0;
+        virtual SeguidoresUniversidades get(std::string key) = 0;
+        virtual SeguidoresUniversidades remove(std::string key) = 0;
 
         virtual int size() = 0;
         virtual bool empty() = 0;
 
-        SeguidoresUniversidades& operator[](long long key){
+        SeguidoresUniversidades operator[](long long key){
             return this->get(key);
         }
 
-        SeguidoresUniversidades& operator[](std::string key){
+        SeguidoresUniversidades operator[](std::string key){
             return get(key);
         }
 
--- MapAbierto.h
+++ MapAbierto.h
@@ -1,10 +1,89 @@
+#include <vector>
+#include <list>
+#include <iostream>
 #include "MapADT.h"
 
 class MapAbierto : public MapADT{
     private:
-
+        int seguidores_totales;
     public:
+        std::vector<std::list<SeguidoresUniversidades>> * contenedor_seguidores;
+
         MapAbierto(opcion_de_clave opcion) : MapADT(opcion){
-            
+            contenedor_seguidores = new std::vector<std::list<SeguidoresUniversidades>>(3);
+            seguidores_totales = 0;
         }
+
+        void put(SeguidoresUniversidades usuarios){
+            if (opcion == se_usa_user_id)
+                _put_with_longlong(usuarios);
+            else
+                _put_with_longlong(usuarios);
+            
+            _verificar_cantidad_ocupada();
+
+        }
+        
+        SeguidoresUniversidades get(long long key){
+            int index = _hashf1(key);
+            SeguidoresUniversidades nullse;
+            nullse.user_name = "Invalid user name";
+            std::list<SeguidoresUniversidades> lista = (*contenedor_seguidores)[index];
+            for (auto elem : lista){
+                if (elem.user_id == key)
+                    return elem;
+            }
+
+            return nullse;
+        }
+
+
+        int size(){
+            return seguidores_totales;
+        }
+
+
+        bool empty(){
+            return (size() == 0) ? true :  false;
+        };
+    private:
+        void _put_with_string(SeguidoresUniversidades usuarios){
+        }
+
+        void _put_with_longlong(SeguidoresUniversidades usuarios){
+            int index = _hashf1(usuarios.user_id);
+
+            (*contenedor_seguidores)[index].push_back(usuarios);
+
+            seguidores_totales++;
+        }
+        
+        float _porcentaje_llenado(){
+            return float(seguidores_totales) / float(contenedor_seguidores->size());
+        }
+
+        void _verificar_cantidad_ocupada(){
+            if (_porcentaje_llenado() < 0.9)
+                return;
+            
+            std::vector<std::list<SeguidoresUniversidades>> * viejo_contenedor_seguidores = this->contenedor_seguidores;
+            this->contenedor_seguidores = new std::vector<std::list<SeguidoresUniversidades>>(contenedor_seguidores->size() * 2);
+            seguidores_totales = 0;
+            //hacemos rehashing
+            for (auto lista : *viejo_contenedor_seguidores){
+                while (!lista.empty()){
+                    SeguidoresUniversidades elemento = lista.back();
+                    lista.pop_back();
+                    put(elemento);
+                }
+            }
+            delete viejo_contenedor_seguidores;
+            std::cout << "se llamo\n";
+
+        }
+
+        int _hashf1(int k) {
+            return k % (*contenedor_seguidores).size();
+        }
+
 };
