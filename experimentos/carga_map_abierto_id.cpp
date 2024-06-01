6a590da88b71011c1070e4f24d3f5ce6eb8068fc
karantooo <ctomasalvarezn@gmail.com>
2024-06-01 16:19:59 -0400

    feat: Se agregan los primeros experimentos de carga para el map cerrado

---
+++ experimentos/carga_map_abierto_id.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include "../MapAbierto.h"
+#include "../CargarDatos.h"
+
+
+int main(int argc, char** argv){
+    MapAbierto map(se_usa_user_id);
+    CargarDatos datos(&map, "universities_followers.csv");
+    
+
+    
+
+    datos.cargar_datos(std::stoi(argv[1]), std::stoi(argv[2]));
+
+    //medimos la carga de datos
+
+    auto start = std::chrono::high_resolution_clock::now();
+    datos.cargar_map();
+    auto end = std::chrono::high_resolution_clock::now();
+    double running_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+    
+    running_time *= 1e-9;
+    std::cout << argv[1] << ";" << argv[2] << ";" << running_time << std::endl;
+
+    return 0;
+}
