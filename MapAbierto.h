77cb851371881e844874a92b0a9d6132ca3b9287
karantooo <ctomasalvarezn@gmail.com>
2024-06-01 01:29:41 -0400

    docs: documentacion mapAbierto

--- MapAbierto.h
+++ MapAbierto.h
@@ -5,13 +5,22 @@
 #include "MapADT.h"
 #include "interesting_prime_numbers.h"
 
+
+/**
+ * @brief Implementacion de MapADT utilizando hashing abierto
+ * 
+ */
 class MapAbierto : public MapADT{
     private:
         int seguidores_totales;
         std::vector<long long>::iterator siguiente_tam_contenedor;
         std::vector<std::list<SeguidoresUniversidades>> contenedor_seguidores;
     public:
-
+        /**
+         * @brief Construye un nuevo MapAbierto
+         * 
+         * @param opcion campo que se utilizara como key
+         */
         MapAbierto(opcion_de_clave opcion) : MapADT(opcion){
             siguiente_tam_contenedor = numeros_primos_interesantes.begin();
             contenedor_seguidores.resize(*siguiente_tam_contenedor);
@@ -19,6 +28,13 @@ class MapAbierto : public MapADT{
             seguidores_totales = 0;
         }
 
+        /**
+         * @brief implementacion de put de mapADT
+         * Dependiendo de cual sea la key se llama a una funcion distinta para añadirla al map
+         * Despues se verifica si se nececita aumentar el espacio 
+         * 
+         * @param usuarios usuario a añadir
+         */
         void put(SeguidoresUniversidades usuarios) override {
             if (opcion == se_usa_user_id)
                 _put_with_longlong(usuarios);
@@ -29,6 +45,13 @@ class MapAbierto : public MapADT{
 
         }
         
+        /**
+         * @brief Metodo para obtener un usuario a partir de su user_id
+         * En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         * @param key user_id del usuario a buscar
+         * @return SeguidoresUniversidades seguidor buscado
+         * @return En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         */
         SeguidoresUniversidades get(long long key) override {
             SeguidoresUniversidades nullse;
             nullse.user_name = "Invalid user name";
@@ -44,6 +67,13 @@ class MapAbierto : public MapADT{
             return nullse;
         }
 
+        /**
+         * @brief Implementacion clase para eliminar un elemento del map a partir de user_id
+         * En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         * @param key user_id del usuario a eliminiar
+         * @return SeguidoresUniversidades el usuario eliminado.
+         * @return En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         */
         SeguidoresUniversidades remove(long long key) override {
             SeguidoresUniversidades nullse;
             nullse.user_name = "Invalid user name";
@@ -66,6 +96,13 @@ class MapAbierto : public MapADT{
 
         }
 
+        /**
+         * @brief Metodo para obtener un usuario a partir de su user_name
+         * En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         * @param key user_name del usuario a buscar
+         * @return SeguidoresUniversidades seguidor buscado
+         * @return En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         */
         SeguidoresUniversidades get(std::string key) override {
             SeguidoresUniversidades nullse;
             nullse.user_name = "Invalid user name";
@@ -82,6 +119,14 @@ class MapAbierto : public MapADT{
 
 
         }
+        
+        /**
+         * @brief Implementacion clase para eliminar un elemento del map a partir de user_name
+         * 
+         * @param key user_name del usuario a eliminiar
+         * @return SeguidoresUniversidades el usuario eliminado.
+         * @return En caso de que algo salga mal se va a devolver un usuario con el nombre Invalid user name
+         */
         SeguidoresUniversidades remove(std::string key) override {
             SeguidoresUniversidades nullse;
             nullse.user_name = "Invalid user name";
@@ -103,12 +148,21 @@ class MapAbierto : public MapADT{
 
         }
 
-
+        /**
+         * @brief implementacion de size, para obtener la cantidad de elementos del map
+         * 
+         * @return int cantidad de elementos
+         */
         int size() override {
             return seguidores_totales;
         }
 
-
+        /**
+         * @brief se verifica si el map esta vacio o no
+         * 
+         * @return true esta vacio
+         * @return false no esta vacio
+         */
         bool empty() override {
             return (size() == 0) ? true :  false;
         };
@@ -117,7 +171,14 @@ class MapAbierto : public MapADT{
 
         }
     private:
+        //funciones privadas
 
+
+        /**
+         * @brief funcion para insertar un elemento cuando se esta en modo user_id
+         * 
+         * @param usuarios usuario a insertar
+         */
         void _put_with_longlong(SeguidoresUniversidades usuarios){
             int index = _hashf1(usuarios.user_id);
 
@@ -126,6 +187,11 @@ class MapAbierto : public MapADT{
             seguidores_totales++;
         }
         
+        /**
+         * @brief funcion para insertar un elemento cuando se esta en modo user_name
+         * 
+         * @param usuarios usuario a insertar
+         */
         void _put_with_string(SeguidoresUniversidades usuarios){
             int index =  _hashf1(_acumulacion_polinomial(usuarios.user_name, 33));
 
@@ -134,10 +200,22 @@ class MapAbierto : public MapADT{
             seguidores_totales++;
         }
 
+
+        /**
+         * @brief Devuelve el porcentaje de llenado del map
+         * Se define como porcentaje de llenado la division entre los seguidores insertados en el map
+         * y el tamanio actual del contenedor
+         * @return float porcentaje de llenado
+         */
         float _porcentaje_llenado(){
             return float(seguidores_totales) / float(contenedor_seguidores.size());
         }
 
+        /**
+         * @brief esta funcion aniade espacios al map en caso de que el porcentaje de llenado sea muy alto (0.9)
+         * Si el map esta proximo a llenarse se rellena de nuevo el map.
+         * Posterior a esto se procede a hacer un rehash
+         */
         void _verificar_cantidad_ocupada(){
             if (_porcentaje_llenado() < 0.9)
                 return;
@@ -159,10 +237,29 @@ class MapAbierto : public MapADT{
 
         }
 
+        /**
+         * @brief funcion para generar el hash correspondiente dependiendo de un valor numerico
+         * 
+         * @param k valor numerico a hashear
+         * @return int valor ya hasheado
+         */
         int _hashf1(int k) {
             return k % (contenedor_seguidores).size();
         }
 
+        /**
+         * @brief funcion para hacer el hashing inicial de un string
+         * Posteriormente al hashing se tiene que pasar el resultado por otra funcion como hashf1.
+         * 
+         * Utilizamos acumulacion polinomial para hashear el string
+         * 
+         * Esto es la sumatoria de xn * a^n
+         * Con xn el caracter xn de la palabra y a la constante a utilizar
+         * 
+         * @param word palabra a hashear
+         * @param constante la constante que se utilzira para hacer el hashing
+         * @return int 
+         */
         int _acumulacion_polinomial(std::string word, int constante){
             int resultado = 0;
             for (auto it = word.rbegin(); it != word.rend(); it++){
