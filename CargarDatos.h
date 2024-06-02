dd5b4aeaed86003cfc874df1d5177028e6d89d8b
karantooo <ctomasalvarezn@gmail.com>
2024-06-01 21:26:57 -0400

    feat: se añade obtener llave random

--- CargarDatos.h
+++ CargarDatos.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <random>
 #include "MapADT.h"
 #include "SeguidoresUniversidades.h"
 
@@ -17,6 +18,9 @@ class CargarDatos{
         char delimiter;
         std::string ruta;
         std::ifstream file;
+        std::vector<std::string> keys_user_name;
+        std::vector<long long> keys_user_id;
+
     public:
 
         /**
@@ -96,6 +100,77 @@ class CargarDatos{
             }
         }
 
+         /**
+         * @brief Se cargan los datos cargados desde cargar_datos pero recuerda las keys
+         * Se separa ambas funciones cargar_datos y cargar_map para no tomar en consideracion el tiempo que toma
+         * en leer el archivo y asi reducir el ruido dentro de nuestros datos.
+         * Se recuerdan las keys para propositos de medir el tiempo de las operaciones get
+         */
+        void cargar_map_recordando_keys(){
+            std::vector<std::string> answer; 
+            SeguidoresUniversidades seguidor;
+            int contador = 0;
+            for (std::string linea : lineas_datos){
+                answer = this->_strip(linea);
+                seguidor.university = answer[0];
+                seguidor.user_id = stod(answer[1]);
+                seguidor.user_name = answer[2];
+                seguidor.number_tweets = stod(answer[3]);
+                seguidor.friends_count = stod(answer[4]);
+                seguidor.followers_count = stod(answer[5]);
+                seguidor.created_at = answer[6]; 
+
+                keys_user_name.push_back(seguidor.user_name);
+                keys_user_id.push_back(seguidor.user_id);
+
+                map->put(seguidor);
+
+            }
+        }
+
+        std::string get_random_user_name(){
+            if (keys_user_name.size() == 0)
+                return "invalido";
+            // Semilla para el generador de números aleatorios
+            std::random_device rd;
+            std::mt19937 gen(rd());
+
+            // Límites superior e inferior
+            int lower_bound = 0;
+            int upper_bound = keys_user_name.size() - 1;
+
+            // Distribución uniforme entre los límites
+            std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
+
+            // Generar número aleatorio
+            int random_number = distribution(gen);
+
+
+            return keys_user_name[random_number];
+        }
+
+        long long get_random_user_id(){
+            if (keys_user_id.size() == 0)
+                return -1;
+            // Semilla para el generador de números aleatorios
+            std::random_device rd;
+            std::mt19937 gen(rd());
+
+            // Límites superior e inferior
+            int lower_bound = 0;
+            int upper_bound = keys_user_id.size() - 1;
+
+            // Distribución uniforme entre los límites
+            std::uniform_int_distribution<int> distribution(lower_bound, upper_bound);
+
+            // Generar número aleatorio
+            int random_number = distribution(gen);
+
+
+            return keys_user_id[random_number];
+        }
+
+
         ~CargarDatos(){
             file.close();
         }
@@ -103,9 +178,9 @@ class CargarDatos{
         private:
 
             /**
-             * @brief Funcion que toma las lineas esctraidas del csv y las separa en un array a partir del delimeter
+             * @brief Funcion que toma las lineas extraidas del csv y las separa en un array a partir del delimeter
              * 
-             * @param linea_actual linea qye se va a separar
+             * @param linea_actual linea que se va a separar
              * @return std::vector<std::string> linea separada por el delimiter
              */
             std::vector<std::string> _strip (std::string linea_actual){
