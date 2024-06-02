d1663bf1f6856107052633cb26233543bad7e8ac
karantooo <ctomasalvarezn@gmail.com>
2024-06-01 21:46:53 -0400

    feat: se fusiona el experimento para la carga map abierto

--- experimentos/carga_map_abierto.cpp
+++ experimentos/carga_map_abierto.cpp
@@ -22,7 +22,23 @@ int main(int argc, char** argv){
     double running_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
     
     running_time *= 1e-9;
-    std::cout << argv[1] << ";" << argv[2] << ";" << running_time << std::endl;
+
+
+    MapAbierto map2(se_usa_user_id);
+    CargarDatos datos2(&map2, "universities_followers.csv");
+    
+    datos2.cargar_datos(std::stoi(argv[1]), std::stoi(argv[2]));
+
+    //medimos la carga de datos
+
+    start = std::chrono::high_resolution_clock::now();
+    datos2.cargar_map();
+    end = std::chrono::high_resolution_clock::now();
+    double running_time_2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+    
+    running_time_2 *= 1e-9;
+    std::cout << argv[1] << ";" << argv[2] << ";" << running_time << ";" << running_time_2 << std::endl;
+
 
     return 0;
 }
