c119f368a6090c4fef4bc1b1ae33518e79a262c1
karantooo <ctomasalvarezn@gmail.com>
2024-06-06 19:09:30 -0400

    feat: ultimo test datos

--- experimentos/run_experiments_carga_tiempo.sh
+++ experimentos/run_experiments_carga_tiempo.sh
@@ -4,7 +4,7 @@
 echo "limite superior;tiempo clave user_name linear_probing;tiempo clave user_id linear_probing;tiempo clave user_name quadratic;tiempo clave user_id quadratic;tiempo clave user_name double;tiempo clave user_id double;tiempo clave user_name abierto;tiempo clave user_id abierto;tiempo clave user_name stl;tiempo clave user_id stl"
 
 # Repetiremos los experimentos 30 veces
-for (( c=1; c<=3000; c++ ))
+for (( c=1; c<=100; c++ ))
 do
     # Ejecutamos la primera versión de heapsort con 5 entradas distintas
     for n in 1000 5000 10000 15000 20000
